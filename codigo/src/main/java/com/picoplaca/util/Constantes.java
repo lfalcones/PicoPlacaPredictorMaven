73988123724d877e1db8ff96bf6d28f6d392581c
lfalcones <lfalcones@vimeworks.com>
2018-04-01 21:00:15 -0500

    para otra pruebita

--- codigo/src/main/java/com/picoplaca/util/Constantes.java
+++ codigo/src/main/java/com/picoplaca/util/Constantes.java
@@ -5,6 +5,8 @@ import java.time.LocalTime;
 
 public class Constantes {
 
+	/**Comentario simple para pruebas
+	*/
 	public static LocalTime START_MORNING = LocalTime.of(7, 0);
 	public static LocalTime END_MORNING = LocalTime.of(9, 30);
 	public static LocalTime START_AFTERNOON = LocalTime.of(16,0);
