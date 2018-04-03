4ae7b85d1bea62eb51c0ce7ef7abe6f8da9d417c
Luis Falcones <luisfalcones@gmail.com>
2018-04-02 22:11:29 -0500

    practica de merge en el mismo archivo

--- codigo/src/main/java/com/picoplaca/util/Constantes.java
+++ codigo/src/main/java/com/picoplaca/util/Constantes.java
@@ -11,4 +11,6 @@ public class Constantes {
 	public static LocalTime END_MORNING = LocalTime.of(9, 30);
 	public static LocalTime START_AFTERNOON = LocalTime.of(16,0);
 	public static LocalTime END_AFTERNOON = LocalTime.of(19, 30);
+	public static LocalTime START_AFTERNOON_2 = LocalTime.of(16,0);
+	public static LocalTime END_AFTERNOON_2 = LocalTime.of(19, 30);
 }
